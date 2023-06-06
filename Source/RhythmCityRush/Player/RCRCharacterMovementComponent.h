38f588ac02e8056700f4d02116a6e4f47631b902
Taisen Colcher <TaisenColcher@Gmail.com>
2023-06-06 11:22:10 -0400

    Create RCRCharacterMovementComponent Class
    Created UCharacterMovementComponent derived class.

---
+++ Source/RhythmCityRush/Player/RCRCharacterMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "RCRCharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RHYTHMCITYRUSH_API URCRCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
