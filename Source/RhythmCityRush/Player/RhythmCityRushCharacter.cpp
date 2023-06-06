599ec4fdd09be10bbcf46d640c59be267723a940
Taisen Colcher <TaisenColcher@Gmail.com>
2023-06-06 11:19:44 -0400

    Implement Movement
    Player can move, jump, and rotate camera.
    Created BP_RhythmCityRushCharacter.
    Set new character to BP_GameModeBase.
    Created Dev_Map for testing.

--- Source/RhythmCityRush/Player/RhythmCityRushCharacter.cpp
+++ Source/RhythmCityRush/Player/RhythmCityRushCharacter.cpp
@@ -3,18 +3,50 @@
 
 #include "RhythmCityRushCharacter.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
 // Sets default values
 ARhythmCityRushCharacter::ARhythmCityRushCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+
+	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+	
 }
 
 // Called when the game starts or when spawned
 void ARhythmCityRushCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Add Input Mapping Context
+	if(APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 	
 }
 
@@ -30,5 +62,76 @@ void ARhythmCityRushCharacter::SetupPlayerInputComponent(UInputComponent* Player
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// Setup Action Bindings
+	if(UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ARhythmCityRushCharacter::Move);
+
+		// Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ARhythmCityRushCharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ARhythmCityRushCharacter::StopJumping);
+
+		// Looking
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ARhythmCityRushCharacter::Look);
+	}
+
+}
+
+void ARhythmCityRushCharacter::Move(const FInputActionValue& Value)
+{
+	
+	// Input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	
+	if(Controller != nullptr)
+	{
+		// Find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator  YawRotation(0, Rotation.Yaw, 0);
+
+		// Get Forward Vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		// Get Right Vector
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		
+		if(MovementVector.Y < 0)
+		{
+			// Increase to bring character to a stop when pulled back
+			GetCharacterMovement()->BrakingDecelerationWalking = 1500;
+		}
+		else
+		{
+			AddMovementInput(ForwardDirection, MovementVector.Y);
+			AddMovementInput(RightDirection, MovementVector.X);
+			GetCharacterMovement()->BrakingDecelerationWalking = 0;
+		}
+	}
+}
+
+void ARhythmCityRushCharacter::Look(const FInputActionValue& Value)
+{
+
+	// Input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if(Controller != nullptr)
+	{
+		// Add Yaw and Pitch Input to Controller
+		AddControllerPitchInput(LookAxisVector.Y); 
+		AddControllerYawInput(LookAxisVector.X);
+	}
+	
+}
+
+void ARhythmCityRushCharacter::Jump()
+{
+	Super::Jump();
+}
+
+void ARhythmCityRushCharacter::StopJumping()
+{
+	Super::StopJumping();
 }
 
--- Source/RhythmCityRush/Player/RhythmCityRushCharacter.h
+++ Source/RhythmCityRush/Player/RhythmCityRushCharacter.h
@@ -3,7 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "RhythmCityRushCharacter.generated.h"
 
 UCLASS()
@@ -11,6 +14,16 @@ class RHYTHMCITYRUSH_API ARhythmCityRushCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	// Components
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	UCameraComponent* FollowCamera;
+	
+	
 public:
 	// Sets default values for this character's properties
 	ARhythmCityRushCharacter();
@@ -25,5 +38,34 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+
+	// Input
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* LookAction;
+	
+	// Called for Movement Input
+	void Move(const FInputActionValue& Value);
+
+	// Called for Look Input
+	void Look(const FInputActionValue& Value);
+
+	virtual void Jump() override;
+
+	virtual void StopJumping() override;
+
+public:
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
 };
