ae35b86936ff94434a9986e53a5f022bffcbe50a
Taisen Colcher <TaisenColcher@Gmail.com>
2023-06-06 14:33:02 -0400

    Add Print Macro To RhythmCityRush.h
    Added a quick SCREENPRINT(x) macro for debugging.

--- Source/RhythmCityRush/RhythmCityRush.h
+++ Source/RhythmCityRush/RhythmCityRush.h
@@ -4,6 +4,14 @@
 
 #include "CoreMinimal.h"
 
+// Helper Macros
+#if 0
+float MacroDuration = 5.f;
+#define SCREENPRINT(x) GEngine->AddOnScreenDebugMEssage(-1, MacroDuration ? MacroDuration : -1.0f, FColor::Black, x);
+#else
+#define SCREENPRINT(x)
+#endif
+
 // Character Classes
 class ARhythmCityRushCharacter;
 class URCRCharacterMovementComponent;
