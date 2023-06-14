cbb258b5d2a7a52c2a8cfc96e410cb688d2bc15e
Taisen Colcher <TaisenColcher@Gmail.com>
2023-06-14 14:40:41 -0400

    Add UMaterialInterface Reference To TaggingSystemComponent
    Added a UMaterialnterface* for the decal to be spawned.

--- Source/RhythmCityRush/Player/TaggingSystemComponent.h
+++ Source/RhythmCityRush/Player/TaggingSystemComponent.h
@@ -24,5 +24,9 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UPROPERTY(EditAnywhere, Category = "Graffiti")
+	UMaterialInterface* TagDecalToSpawn;
+
+	
+	
 };
