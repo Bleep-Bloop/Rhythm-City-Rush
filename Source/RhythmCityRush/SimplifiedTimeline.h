9b697157d1cdb6c84159be798c3d6c1010505161
Taisen Colcher <TaisenColcher@Gmail.com>
2023-06-16 00:53:48 -0400

    Add SkipToEnd Functino On SimplifiedTimeline.
    Added a function that skips the timeline to the end and broadcasts OnTimerFinished. This is used in the Grind Controller Blueprint to handle jumping while grinding.

--- Source/RhythmCityRush/SimplifiedTimeline.h
+++ Source/RhythmCityRush/SimplifiedTimeline.h
@@ -76,6 +76,8 @@ public:
  
     UFUNCTION(BlueprintCallable, Category = "Simplified Timeline")
         void Reverse();
- 
+
+    UFUNCTION(BlueprintCallable, Category = "Simplified Timeline")
+        void SkipToEnd();
  
 };
